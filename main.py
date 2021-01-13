6e3efc4ac655c71d1f1693eaf5ddaf0a7e2b0478
Riprock <8990377+Riprock@users.noreply.github.com>
2021-01-13 16:15:18 -0500

    Starting to fill information
    Starting to fill out how the data is going to get into the node

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
 from neo4j.exceptions import ServiceUnavailable
 import logging
+import xml.etree.ElementTree as ET
 
 class App:
     def __init__(self, url, user, password):
@@ -28,6 +29,28 @@ class App:
             raise
 
 if __name__ == "__main__":
+    root = ET.parse('test1.xml').getroot()
     app = App("bolt://localhost:7687", "neo4j", "sysmon")
     app.proc_start("crss.exe", "2")
     app.close()
+    UtcTime = root[1][1].text
+    ProcessGuid = root[1][2].text
+    ProcessId = root[1][3].text
+    Image = root[1][4].text
+    FileVersion = root[1][5].text
+    Description = root[1][6].text
+    Product = root[1][7].text
+    Company = root[1][8].text
+    OriginalFileName = root[1][9].text
+    CommandLine = root[1][10].text
+    CurrentDirectory = root[1][11].text
+    User = root[1][12].text
+    LogonGuid = root[1][13].text
+    LogonId = root[1][14].text
+    TerminalSessionId= root[1][15].text
+    IntegrityLevel = root[1][16].text
+    Hashes = root[1][17].text
+    ParentProcessGuid = root[1][18].text
+    ParentProcessId = root[1][19].text
+    ParentImage = root[1][20].text
+    ParentCommandLine = root[1][21].text
